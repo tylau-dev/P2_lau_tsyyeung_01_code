f5ffbf7a16a3e80f60ccbcc7a8fbd41cee20b9ee
tylau-dev <lautsyyeung@gmail.com>
2022-02-17 16:14:13 +0100

    added sorting functionality

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -5,16 +5,18 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 import com.hemebiotech.analytics.Interface.IAnalyticsCounter;
 
 public class AnalyticsCounter implements IAnalyticsCounter {
 	private String filepath;
-	public ArrayList<String> readerResult = new ArrayList<String>();
-	public Map<String, Integer> SymptomsCountList = new HashMap<String, Integer>();
+	public ArrayList<String> symptomsList = new ArrayList<String>();
+	public TreeMap<String, Integer> symptomsCountMap = new TreeMap<String, Integer>();
 	
 	public AnalyticsCounter (String filepath) {
 		this.filepath = filepath;
@@ -27,7 +29,7 @@ public class AnalyticsCounter implements IAnalyticsCounter {
 				String line = reader.readLine();
 				
 				while (line != null) {
-					readerResult.add(line);
+					symptomsList.add(line);
 					line = reader.readLine();
 				}
 				reader.close();
@@ -38,21 +40,25 @@ public class AnalyticsCounter implements IAnalyticsCounter {
 	}
 		
 	public void CountSymptoms() {
-		for (int i = 0; i < readerResult.size(); i++) {
-			String symptom = readerResult.get(i);
-			Integer existingMapCount = SymptomsCountList.get(readerResult.get(i));
+		Collections.sort(symptomsList);
+		System.out.println(symptomsList);
+		for (int i = 0; i < symptomsList.size(); i++) {
+//			System.out.println(symptomsList.get(i));
+			String symptom = symptomsList.get(i);
+			Integer existingMapCount = symptomsCountMap.get(symptom);
 			if (existingMapCount != null) 
-				{SymptomsCountList.put(symptom, existingMapCount + 1);}
-			else {SymptomsCountList.put(symptom, 1);}					
+				{symptomsCountMap.put(symptom, existingMapCount + 1);}
+			else {symptomsCountMap.put(symptom, 1);}
+			System.out.println(symptomsCountMap);
 		}
-		System.out.println(SymptomsCountList);		
+//		System.out.println(symptomsCountMap);		
 	}
 
 	public void WriteSymptoms() {
 		try {
 			FileWriter writer = new FileWriter ("result.out");
 			
-			for (Map.Entry<String, Integer> entry : SymptomsCountList.entrySet()) {
+			for (Map.Entry<String, Integer> entry : symptomsCountMap.entrySet()) {
 				writer.write(entry.getKey() + "=" + String.valueOf(entry.getValue()));
 			}
 		
